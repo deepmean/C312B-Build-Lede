725e3db6433315b120b17c1ce2e896fa30a1c17e
deepmean <deepmean@gmail.com>
2025-01-07 15:32:59 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+opkg install nlbwmon collectd collectd-mod-cpu collectd-mod-interface collectd-mod-iwinfo collectd-mod-load collectd-mod-memory collectd-mod-network collectd-mod-rrdtool iperf3 ipset iptables-mod-conntrack-extra iptables-mod-extra iptables-mod-ipopt iptables-mod-iprange iptables-mod-socket iptables-mod-tproxy libaio libip4tc2 libip6tc2 libiperf3 libipset13 libltdl7 liblzo2 libncurses6 libopenssl-conf libopenssl3 libpopt0 libpthread librrd1 librt libss2 dropbear
+
+
+
+
+
+
+
+
